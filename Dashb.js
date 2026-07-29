6edc8e76ed07094b0688dbdbe8e424cd1e05685e
iki1uc <entertrainer@web.de>
2026-07-30 01:52:14 +0200

    Add warp and curvature properties to ION_DASHBOARD

--- Dashb.js
+++ Dashb.js
@@ -2,6 +2,8 @@ export function ION_DASHBOARD(ion){
     return {
         mode: ion.mode,
         pulse: ion.pulse,
+        warp: ion.warp,
+        curvature: ion.curvature,
         charge: ion.charge,
         state: ion.state
     };
